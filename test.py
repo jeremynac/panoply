c7264f87177bc1792be518bebf3d05fc14b63e31
jeremynac <jeremynac@hotmail.fr>
2021-11-11 12:37:08 +0100

    Add search by default

--- test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-from fast_autocomplete import AutoComplete
-words = {'book': {}, 'burrito': {}, 'pizza': {}, 'pasta':{}}
-autocomplete = AutoComplete(words=words)
-autocomplete.search(word='b', max_cost=3, size=3)
-autocomplete.search(word='bu', max_cost=3, size=3)
-autocomplete.search(word='barrito', max_cost=3, size=3)
